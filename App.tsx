5553d6182a8c3b4b80f52769f3936fa6e0f778b4
Raj-Timalsina <rajtimalsina2062@gmail.com>
2025-05-06 18:13:20 +0545

    Works the basic logic

--- App.tsx
+++ App.tsx
@@ -6,16 +6,26 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [bgColor, setBgColor] = useState('#ffffff');
+
+  const generateBg = () => {
+    const hexRange = '0123456789abcdef';
+    let hexCode = '#';
+    for (let i = 0; i < 6; i++) {
+      hexCode += hexRange[Math.floor(Math.random() * 16)];
+    }
+    setBgColor(hexCode);
+  };
   return (
     <>
-      <StatusBar backgroundColor={'#000000'} />
-      <View>
-        <TouchableOpacity style={[styles.container]}>
-          <View>
-            <Text>Press me</Text>
+      <StatusBar backgroundColor={'#000000'} barStyle="light-content" />
+      <View style={{flex: 1, backgroundColor: bgColor}}>
+        <TouchableOpacity style={[styles.container]} onPress={generateBg}>
+          <View style={styles.button}>
+            <Text style={[styles.buttonText, {color: bgColor}]}>Press me</Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -28,5 +38,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  button: {
+    borderRadius: 12,
+    backgroundColor: '#FFC107',
+    padding: 20,
+    elevation: 4,
+  },
+  buttonText: {
+    fontSize: 24,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    textShadowColor: 'black',
+    textShadowOffset: {width: 1, height: 1},
+    textShadowRadius: 1,
   },
 });
