0abce5e36fe3e7437afa31946f381fd90c66b8b1
Raj-Timalsina <rajtimalsina2062@gmail.com>
2025-05-06 21:08:15 +0545

    added shapes

--- App.tsx
+++ App.tsx
@@ -2,55 +2,124 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  Touchable,
   TouchableOpacity,
   View,
 } from 'react-native';
 import React, {useState} from 'react';
 
 export default function App() {
+  type Shape = {
+    top: number;
+    left: number;
+    id: number;
+    color: string;
+    type: 'square' | 'circle';
+  };
+
   const [bgColor, setBgColor] = useState('#ffffff');
+  const [shapes, setShapes] = useState<Shape[]>([]);
 
   const generateBg = () => {
+    const shapeType = Math.random() < 0.5 ? 'square' : 'circle';
     const hexRange = '0123456789abcdef';
-    let hexCode = '#';
+    let hexCode1 = '#';
+    let hexCode2 = '#';
+
     for (let i = 0; i < 6; i++) {
-      hexCode += hexRange[Math.floor(Math.random() * 16)];
+      hexCode1 += hexRange[Math.floor(Math.random() * 16)];
+      hexCode2 += hexRange[Math.floor(Math.random() * 16)];
     }
-    setBgColor(hexCode);
+
+    const top = Math.floor(Math.random() * 800);
+    const left = Math.floor(Math.random() * 300);
+
+    setBgColor(hexCode1);
+
+    const newShape: Shape = {
+      top,
+      left,
+      id: Date.now(),
+      color: hexCode2,
+      type: shapeType,
+    };
+
+    setShapes(prev => [...prev, newShape]);
   };
+
+  const reset = () => {
+    setShapes([]);
+    setBgColor('#ffffff');
+  };
+
   return (
     <>
       <StatusBar backgroundColor={'#000000'} barStyle="light-content" />
-      <View style={{flex: 1, backgroundColor: bgColor}}>
-        <TouchableOpacity style={[styles.container]} onPress={generateBg}>
-          <View style={styles.button}>
+      <View style={[styles.fullscreen, {backgroundColor: bgColor}]}>
+        <View style={styles.controls}>
+          <TouchableOpacity style={styles.button} onPress={generateBg}>
             <Text style={[styles.buttonText, {color: bgColor}]}>Press me</Text>
-          </View>
-        </TouchableOpacity>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={reset}>
+            <Text style={[styles.buttonText, {color: bgColor}]}>Reset</Text>
+          </TouchableOpacity>
+        </View>
+
+        {shapes.map(item => (
+          <View
+            key={item.id}
+            style={[
+              item.type === 'square' ? styles.square : styles.circle,
+              {
+                top: item.top,
+                left: item.left,
+                borderColor: item.color,
+              },
+            ]}
+          />
+        ))}
       </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  fullscreen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  controls: {
+    flexDirection: 'row',
+  },
   button: {
     borderRadius: 12,
     backgroundColor: '#FFC107',
-    padding: 20,
+    padding: 16,
     elevation: 4,
+    margin: 20,
   },
   buttonText: {
-    fontSize: 24,
-    textTransform: 'uppercase',
+    fontSize: 20,
     fontWeight: 'bold',
+    textTransform: 'uppercase',
     textShadowColor: 'black',
     textShadowOffset: {width: 1, height: 1},
     textShadowRadius: 1,
+  },
+  square: {
+    width: 100,
+    height: 100,
+    borderWidth: 4,
+    borderRadius: 12,
+    backgroundColor: 'transparent',
+    position: 'absolute',
+  },
+  circle: {
+    width: 100,
+    height: 100,
+    borderWidth: 4,
+    borderRadius: 50,
+    backgroundColor: 'transparent',
+    position: 'absolute',
   },
 });
