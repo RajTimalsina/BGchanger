8b8818d584c19fd90d58af2836dbac1f1bb6b0ab
Raj-Timalsina <rajtimalsina2062@gmail.com>
2025-05-07 16:43:42 +0545

    More shapes added and angles are added

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import {
   StatusBar,
   StyleSheet,
@@ -5,7 +6,6 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React, {useState} from 'react';
 
 export default function App() {
   type Shape = {
@@ -13,14 +13,18 @@ export default function App() {
     left: number;
     id: number;
     color: string;
-    type: 'square' | 'circle';
+    angle: number;
+    type: 'square' | 'circle' | 'triangle' | 'diamond' | 'ellipse';
   };
 
   const [bgColor, setBgColor] = useState('#ffffff');
   const [shapes, setShapes] = useState<Shape[]>([]);
 
   const generateBg = () => {
-    const shapeType = Math.random() < 0.5 ? 'square' : 'circle';
+    const shapeType = ['square', 'circle', 'triangle', 'diamond', 'ellipse'][
+      Math.floor(Math.random() * 5)
+    ] as Shape['type'];
+
     const hexRange = '0123456789abcdef';
     let hexCode1 = '#';
     let hexCode2 = '#';
@@ -29,9 +33,13 @@ export default function App() {
       hexCode1 += hexRange[Math.floor(Math.random() * 16)];
       hexCode2 += hexRange[Math.floor(Math.random() * 16)];
     }
-
-    const top = Math.floor(Math.random() * 800);
+    let top: number = 0;
+    do {
+      top = Math.floor(Math.random() * 800);
+    } while (top > 300 && top < 500);
     const left = Math.floor(Math.random() * 300);
+
+    const angle = Math.floor(Math.random() * 180);
 
     setBgColor(hexCode1);
 
@@ -40,6 +48,7 @@ export default function App() {
       left,
       id: Date.now(),
       color: hexCode2,
+      angle,
       type: shapeType,
     };
 
@@ -53,7 +62,7 @@ export default function App() {
 
   return (
     <>
-      <StatusBar backgroundColor={'#000000'} barStyle="light-content" />
+      <StatusBar backgroundColor="#000000" barStyle="light-content" />
       <View style={[styles.fullscreen, {backgroundColor: bgColor}]}>
         <View style={styles.controls}>
           <TouchableOpacity style={styles.button} onPress={generateBg}>
@@ -63,20 +72,71 @@ export default function App() {
             <Text style={[styles.buttonText, {color: bgColor}]}>Reset</Text>
           </TouchableOpacity>
         </View>
+        {shapes.map(item => {
+          if (item.type === 'triangle') {
+            return (
+              <View
+                key={item.id}
+                style={[
+                  styles.triangle,
+                  {
+                    top: item.top,
+                    left: item.left,
+                    borderBottomColor: item.color,
+                    transform: [{rotate: `${item.angle} deg`}],
+                  },
+                ]}
+              />
+            );
+          }
+          if (item.type === 'ellipse') {
+            return (
+              <View
+                key={item.id}
+                style={[
+                  styles.ellipse,
+                  {
+                    top: item.top,
+                    left: item.left,
+                    borderColor: item.color,
+                    transform: [{rotate: `${item.angle} deg`}],
+                  },
+                ]}
+              />
+            );
+          }
+          if (item.type === 'diamond') {
+            return (
+              <View
+                key={item.id}
+                style={[
+                  styles.diamond,
+                  {
+                    top: item.top,
+                    left: item.left,
+                    borderColor: item.color,
+                    transform: [{rotate: `${item.angle} deg`}],
+                  },
+                ]}
+              />
+            );
+          }
 
-        {shapes.map(item => (
-          <View
-            key={item.id}
-            style={[
-              item.type === 'square' ? styles.square : styles.circle,
-              {
-                top: item.top,
-                left: item.left,
-                borderColor: item.color,
-              },
-            ]}
-          />
-        ))}
+          return (
+            <View
+              key={item.id}
+              style={[
+                item.type === 'square' ? styles.square : styles.circle,
+                {
+                  top: item.top,
+                  left: item.left,
+                  borderColor: item.color,
+                  transform: [{rotate: `${item.angle} deg`}],
+                },
+              ]}
+            />
+          );
+        })}
       </View>
     </>
   );
@@ -90,6 +150,8 @@ const styles = StyleSheet.create({
   },
   controls: {
     flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
   },
   button: {
     borderRadius: 12,
@@ -119,6 +181,35 @@ const styles = StyleSheet.create({
     height: 100,
     borderWidth: 4,
     borderRadius: 50,
+    backgroundColor: 'transparent',
+    position: 'absolute',
+  },
+  triangle: {
+    width: 0,
+    height: 0,
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    borderLeftWidth: 50,
+    borderRightWidth: 50,
+    borderBottomWidth: 100,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: 'black',
+  },
+  diamond: {
+    width: 100,
+    height: 100,
+    borderWidth: 4,
+    backgroundColor: 'transparent',
+    transform: [{rotate: '45deg'}],
+    position: 'absolute',
+    borderColor: 'black',
+  },
+  ellipse: {
+    width: 120,
+    height: 70,
+    borderWidth: 4,
+    borderRadius: 100,
     backgroundColor: 'transparent',
     position: 'absolute',
   },
